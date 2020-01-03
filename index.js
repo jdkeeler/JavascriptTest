eba48b953915e203a541ae8b2ac5de84a9c45ef5
Joseph Keeler <joseph.keeler@nike.com>
2020-01-03 13:55:06 -0800

    Updated test to be more standard JS

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
-const Questions = class {
-    static run() {
+function run() {
 
         //Complete all problems below.
         //Once done run all problems from here
 
-    }
+}
 
-    //Instructions:
-    //  Given an array of integers, output all numbers appearing more than once.  Only output duplicates
-    //  one time.  A sample input array is provided, but your code should perform for any
-    //  array of any number of integers.
-    static findDuplicates() {
+//Instructions:
+//  Given an array of integers, output all numbers appearing more than once.  Only output duplicates
+//  one time.  A sample input array is provided, but your code should perform for any
+//  array of any number of integers.
+function findDuplicates() {
 
-        //Do NOT modify values below
-        var input_array = [1, 5, 3, 2, 6, 8, 9, 10, 1, 5, 8, 9, 3, 10, 45, 32, 16, 14, 3, 5, 9];
+    //Do NOT modify values below
+    var input_array = [1, 5, 3, 2, 6, 8, 9, 10, 1, 5, 8, 9, 3, 10, 45, 32, 16, 14, 3, 5, 9];
 
-        //YOUR CODE HERE
-
-
-        //OUTPUT DUPLICATE NUMBERS
-        console.log("\n\n-----QUESTION_FINDDUPLICATES-----\n\n");
-
-    }
-
-    //Instructions:
-    //  Given an array of integers, return indices of two numbers that add up to the provided target.
-    //  Return only ONE solution.  You may not use the same index twice.  A sample array is provided,
-    //  but code should perform for any array of any number of integers.
-    static twoSum() {
-
-        //Do NOT modify values below
-        input_array = [1, 5, 2, 0, 4, 11, 9, 6, 12, 7];
-        target_value = 9;
-
-        //YOUR CODE HERE
+    //YOUR CODE HERE
 
 
-        //OUTPUT TWO INDICES
-        console.log("\n\n-----QUESTION_TWOSUM-----\n\n");
-
-    }
-
-    //Instructions:
-    //  Generate 100 random numbers in the range of [1 - 1000], inclusive.
-    //  Then put the numbers into a Map
-    //  Move all the numbers into a Set, from the Map
-    //  Move the numbers into a List, from the Set
-    //  Sort the numbers and then output them with one number on every row
-    static collections() {
-
-        //YOUR CODE HERE
-
-        //OUTPUT THE NUMBERS
-        console.log("\n\n-----QUESTION_COLLECTIONS-----\n\n");
-
-    }
-
+    //OUTPUT DUPLICATE NUMBERS
+    console.log("\n\n-----QUESTION_FINDDUPLICATES-----\n\n");
 
 }
 
-Questions.run();
+//Instructions:
+//  Given an array of integers, return indices of two numbers that add up to the provided target.
+//  Return only ONE solution.  You may not use the same index twice.  A sample array is provided,
+//  but code should perform for any array of any number of integers.
+function twoSum() {
+
+    //Do NOT modify values below
+    input_array = [1, 5, 2, 0, 4, 11, 9, 6, 12, 7];
+    target_value = 9;
+
+    //YOUR CODE HERE
+
+
+    //OUTPUT TWO INDICES
+    console.log("\n\n-----QUESTION_TWOSUM-----\n\n");
+
+}
+
+//Instructions:
+//  Generate 100 random numbers in the range of [1 - 1000], inclusive.
+//  Put all of the numbers into key/value collection
+//  Then put the numbers into an array
+//  Sort the numbers and then output them as a single string with one number on every line
+function collections() {
+
+    //YOUR CODE HERE
+
+    //OUTPUT THE NUMBERS
+    console.log("\n\n-----QUESTION_COLLECTIONS-----\n\n");
+
+}
+
+
+run();
