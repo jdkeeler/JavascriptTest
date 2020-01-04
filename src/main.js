187a0962e9155d9e3d59e6af350606cb9b028f09
Joseph Keeler <joseph.keeler@nike.com>
2020-01-03 16:48:04 -0800

    Made NPM module

---
+++ src/main.js
@@ -0,0 +1,55 @@
+module.exports.run = function() {
+
+    //Complete all problems below.
+    //Once done run all problems from here
+
+
+    //OUTPUT DUPLICATE NUMBERS
+    console.log("\n\n-----QUESTION_FINDDUPLICATES-----\n\n");
+    console.log(findDuplicates());
+
+    //OUTPUT TWO INDICES
+    console.log("\n\n-----QUESTION_TWOSUM-----\n\n");
+    console.log(twoSum());
+
+
+    //OUTPUT THE NUMBERS
+    console.log("\n\n-----QUESTION_COLLECTIONS-----\n\n");
+    console.log(collections());
+}
+
+//Instructions:
+//  Given an array of integers, return an array of all numbers appearing more than once.
+//  Only output duplicates one time.  A sample input array is provided, but your code should
+//  perform for any array of any number of integers.
+
+//  Do NOT modify the default values below
+function findDuplicates(inputArray = [1, 5, 3, 2, 6, 8, 9, 10, 1, 5, 8, 9, 3, 10, 45, 32, 16, 14, 3, 5, 9]) {
+
+    //YOUR CODE HERE
+
+}
+
+//Instructions:
+//  Given an array of integers, return an array of the array indices of two numbers that add up
+//  to the provided target.
+//  Return only ONE solution.  You may not use the same index twice.  A sample array is provided,
+//  but code should perform for any array of any number of integers.
+
+//  Do NOT modify the default values below
+function twoSum(inputArray = [1, 5, 2, 0, 4, 11, 9, 6, 12, 7], targetValue = 9) {
+
+    //YOUR CODE HERE
+
+}
+
+//Instructions:
+//  Generate 100 random numbers in the range of [1 - 1000], inclusive.
+//  Put all of the numbers into key/value collection with a starting key of 1
+//  Then put the numbers into an array
+//  Sort the numbers and then return them as a single string with one number on every line
+function collections() {
+
+    //YOUR CODE HERE
+
+}
